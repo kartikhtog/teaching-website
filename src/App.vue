3f40217b9f05163c7adf26088d46ef722d046406
Kartik Jain <kartikhtog@gmail.com>
2024-03-09 23:56:25 -0800

    Add logo

--- src/App.vue
+++ src/App.vue
@@ -4,82 +4,59 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <v-app>
+      <v-app-bar>
+        <v-app-bar-nav-icon icon="$vuetify">
+        </v-app-bar-nav-icon>
+        <template v-slot:append>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+        <!-- <v-navigation-drawer location="bottom">
+  <v-list-item title="" subtitle="Vuetify"></v-list-item>
+  <v-divider></v-divider>
+  <v-list-item title="List Item 1">
+    <RouterLink to="/">Home</RouterLink>
+  </v-list-item>
+</v-navigation-drawer> -->
+    <!-- <template v-slot:prepend>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    </template> -->
+    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+    <!-- <v-app-bar-title>Big Brain Learning</v-app-bar-title> -->
+  </v-app-bar>
+  <v-main padding="50">
+    <v-card
+        class="mx-auto"
+        color="#26c6da"
+        theme="dark"
+        max-width="400"
+        prepend-icon="mdi-twitter"
+        title="Twitter"
+      >
+        <template v-slot:prepend>
+          <v-icon size="x-large">md-menu</v-icon>
+        </template>
+        </v-card>
+    <!-- <v-btn>
+    <v-icon>mdi-home</v-icon>
+    </v-btn> -->
 
-      <nav>
+    <!-- <v-icon icon="menu"></v-icon> -->
+
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo-2.jpg" width="125" height="125" /> -->
+  </v-main>
+    <!-- <div class="wrapper"> -->
+      <!-- <HelloWorld msg="Big Brain Learning" /> -->
+      <!-- <v-btn>Hi</v-btn> -->
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
-  </header>
-
+    </div> -->
   <RouterView />
+</v-app>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 })
