f4d4d560ffc27ba5a158ef36a5190390fdc22269
Kartik Jain <kartikhtog@gmail.com>
2024-05-07 21:43:36 -0700

    Add vue router, main app

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,42 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
-    <v-app>
-      <v-app-bar>
-        <v-app-bar-nav-icon icon="$vuetify">
-        </v-app-bar-nav-icon>
-        <template v-slot:append>
-          <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template>
-        <!-- <v-navigation-drawer location="bottom">
-  <v-list-item title="" subtitle="Vuetify"></v-list-item>
-  <v-divider></v-divider>
-  <v-list-item title="List Item 1">
-    <RouterLink to="/">Home</RouterLink>
-  </v-list-item>
-</v-navigation-drawer> -->
-    <!-- <template v-slot:prepend>
+  <v-app>
+    <v-navigation-drawer app v-model="nav_bar_closed">
+    </v-navigation-drawer >
+    <v-app-bar elevation="4" absolute>
+      <v-app-bar-nav-icon @click.stop="NavBarClicked"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-app-bar>
 
-    </template> -->
-    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-    <!-- <v-app-bar-title>Big Brain Learning</v-app-bar-title> -->
-  </v-app-bar>
-  <v-main padding="50">
-    <v-card
-        class="mx-auto"
-        color="#26c6da"
-        theme="dark"
-        max-width="400"
-        prepend-icon="mdi-twitter"
-        title="Twitter"
-      >
-        <template v-slot:prepend>
-          <v-icon size="x-large">md-menu</v-icon>
-        </template>
-        </v-card>
-    <!-- <v-btn>
-    <v-icon>mdi-home</v-icon>
-    </v-btn> -->
-
-    <!-- <v-icon icon="menu"></v-icon> -->
-
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo-2.jpg" width="125" height="125" /> -->
-  </v-main>
-    <!-- <div class="wrapper"> -->
-      <!-- <HelloWorld msg="Big Brain Learning" /> -->
-      <!-- <v-btn>Hi</v-btn> -->
-      <!-- <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div> -->
-  <RouterView />
-</v-app>
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  data() {
+      return {
+       title : "App Name Ispsm",
+       nav_bar_closed: true,
+    }
+  },
+  methods: {
+    NavBarClicked(){
+      this.nav_bar_closed = !this.nav_bar_closed;
+    }
+  },
+}
 
+</script>
 <style scoped>
 
 </style>
